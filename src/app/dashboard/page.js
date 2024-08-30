29a1455d337cfd311364adfecb3a464a667c298c
Lucas <velvettouch1004@gmail.com>
2024-08-30 12:27:37 -0500

    main

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h2>Welcome</h2>
+      <h2>Login</h2>
     </main>
   );
 }
